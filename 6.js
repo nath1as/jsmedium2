a46bae5ac2b1975adb1fb000499d851f0a095884
nathias <n@th1.as>
2018-08-10 16:52:48 +0200

    length can be set

---
+++ 6.js
@@ -0,0 +1,21 @@
+var languages = ['JavaScript', 'Ruby', 'Python'];
+console.log(languages);
+console.log(languages.length);
+
+languages.length = 4;
+console.log(languages);
+console.log(languages.length);
+
+languages.length = 1;
+console.log(languages);
+console.log(languages.length);
+
+languages.length = 3;
+console.log(languages);
+console.log(languages.length);
+
+languages.length = 1;
+languages[2] = 'Python';
+console.log(languages);
+console.log(languages[1]);
+console.log(languages.length);
